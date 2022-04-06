0e2d27111804252e7ca8a33e8196550499994c1e
Erik Dell <edellenator@gmail.com>
2022-04-05 20:01:57 -0700

    display issues with title and issue type

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,6 +9,7 @@ var getUserRepos = function (user) {
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
+        // request was successful
         if (response.ok) {
             response.json().then(function(data){
                 displayRepos(data, user);
@@ -16,6 +17,9 @@ var getUserRepos = function (user) {
         } else {
             alert("Error: GitHub User Not Found");
         }
+    })
+    .catch(function(error){
+        alert("Unable to connect to GitHub");
     });
 };
 
@@ -36,6 +40,12 @@ var formSubmitHandler = function(event) {
 userFormEl.addEventListener("submit", formSubmitHandler);
 
 var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found."
+        return;
+    }
+
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
 
